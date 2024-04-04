d3e731ed1a91573db18cd20cb9c8b0458ceecbbd
AlexP141163 <pilyugin.aa@gmail.com>
2024-04-04 22:21:08 +0300

    Final update main.py

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
+# Этот код демонстрирует применение принципа открытости/закрытости:
+# Мы можем легко добавить новые типы оружия, создав классы, наследующие Weapon,
+# и реализующие метод 'attack', не изменяя при этом классы 'Fighter', 'Monster'.
+
 from abc import ABC, abstractmethod
 # Создаем абстрактный класс 'Weapon', который не имеет
 # собственной реализации.
 class Weapon(ABC):
     @abstractmethod     # декоратор, который показывает, что метод 'attack' является абстрактным.
-    def attack(self):   # Является шаблоyом который должены реализовывать подклассы:
+    def attack(self):   # Является шаблоном, который должены реализовывать подклассы:
         pass            # сам метод не выполняет никаких действий.
 
-# Создаем класс оружия 'Sword' - Меч:
+# Создаем класс оружия 'Sword' - Меч по шаблону 'Weapon":
 class Sword(Weapon):
     def attack(self):
         return "Боец наносит удар мечом."
 
-# Создаем класс оружия 'Bow' - Лук:
+# Создаем класс оружия 'Bow' - Лук по шаблону 'Weapon" :
 class Bow(Weapon):
     def attack(self):
         return "Боец наносит удар из лука."
 
 # Создаем класс бойца 'Fighter':
-class Fighter:
+class Fighter():
     def __init__(self):
-        self.weapon = None  # Боец 'Fighter' без оружия:
+        self.weapon = None  # Боец 'Fighter' без оружия по умолчанию:
 
 # Функция смены оружия:
     def changeWeapon(self, weapon):
@@ -28,26 +32,26 @@ class Fighter:
 
     def fight(self):
         if self.weapon is not None:
-            return self.weapon.attack()  # Выбор оружия:
+            return self.weapon.attack()  # Проверка, есть ли 'Fighter' оружия:
         else:
-            return "Боец без оружия не может сражаться!" # Если оружие не выбрано:
+            return "Боец без оружия не может сражаться!, Пусть выберет оружие!" # Если оружие нет:
 
 # Создаем класс 'Monstr':
-class Monster:
+class Monster():
     # Монстр имеет только одно состояние - "жив" или "побежден".
     def __init__(self):
         self.defeated = False
 
-    def defeat(self):
+    def defeat(self):   # Метод поражения -'defeat' Монстра:
         self.defeated = True
         return "Монстр побежден!"
 
 
-# Демонстрация игрового боя
+# Функция демонстрация игрового боя:
 def battle_demo(fighter, monster, weapon):
-    fighter.changeWeapon(weapon)
-    print(fighter.fight())
-    print(monster.defeat())
+    fighter.changeWeapon(weapon)    # Смена оружия:
+    print(fighter.fight())          # Атака Fighter:
+    print(monster.defeat())         # Поражение Monster:
 
 fighter = Fighter()
 monster = Monster()
@@ -61,3 +65,5 @@ battle_demo(fighter, monster, sword)
 monster = Monster()
 # Боец выбирает лук и сражается с монстром
 battle_demo(fighter, monster, bow)
+# вывод сообщения, что у бойца нет оружия:
+battle_demo(fighter, monster, None)
